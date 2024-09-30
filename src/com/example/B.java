c8d62d13f824c8001e55ee712f3973a649c96b65
gaikwadakash2111 <shariq.shaikh@gorillaexpense.com>
2024-09-30 21:09:51 +0530

    Automated user story name

---
+++ src/com/example/B.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class B {
+
+	public static void main(String[] args) {
+		
+		System.out.println("automated test case no tc005");
+
+	}
+
+}
---
+++ src/com/example/C.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class C {
+
+	public static void main(String[] args) {
+		
+		System.out.println("automated tets case tc006");
+		
+	}
+
+}
